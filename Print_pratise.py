7eb4d120669da14b48ca92ce003b6f49a78d8180
nbote <nbotenbote@gmail.com>
2018-09-07 23:18:24 +0000

    练习格式化数据打印

---
+++ Print_pratise.py
@@ -0,0 +1,14 @@
+# 练习格式化打印数据结果
+
+a = 10
+b = +20.5
+c = -20.5
+print('a = %d' % a)
+print('a = %.2f' % a)
+print('b = %2.1f' % b)
+print('b = %08.2f' % b)
+print('b = %+10.2f' % b)
+print('b = %-10.2f' % b)
+print('c = %08.2f' % c)
+print('c = %+10.2f' % c)
+print('c = %-10.2f' % c)
