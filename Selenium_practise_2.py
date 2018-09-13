be6c2f02b516d86ff061381c5240c2dbcd11165b
nbote <nbotenbote@gmail.com>
2018-09-13 20:03:07 +0000

    selenium_2,find_by_name,v1

---
+++ Selenium_practise_2.py
@@ -0,0 +1,14 @@
+# selenium_class 2
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome('/Users/nbote/Downloads/chromedriver')
+driver.fullscreen_window()
+driver.get('http://www.google.com')
+
+time.sleep(5)
+driver.find_element_by_name('q').send_keys('nbote, Good work!')
+
+time.sleep(2)
+driver.quit()
