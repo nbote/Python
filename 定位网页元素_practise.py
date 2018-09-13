a849504c3828ad01f623da003f7bad6560b0677f
nbote <nbotenbote@gmail.com>
2018-09-13 17:44:46 +0000

    爬虫基础，网页定位练习,v1

---
+++ 定位网页元素_practise.py
@@ -0,0 +1,12 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+data=requests.get('https://www.google.com').content
+
+soup=BeautifulSoup(data,'html.parser')
+
+print(soup.body.div.attrs)
+
+
+
