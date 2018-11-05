cb68b03f16e5fc9b23bca5c61e41428f2ee9d577
nbote <nbotenbote@gmail.com>
2018-11-05 17:03:04 +0000

    requests_practise,v2

--- 爬虫练习/import_requests practise.py
+++ 爬虫练习/import_requests practise.py
@@ -1,5 +1,15 @@
 import requests
 
+# Inty版本
+
 text = requests.get('http://www.baidu.com')
 
 print(text.content)
+
+
+# 周莫烦版本
+# from urllib.request import urlopen
+#
+# html=urlopen('http://www.baidu.com').read().decode('utf-8')
+# print(html)
+
